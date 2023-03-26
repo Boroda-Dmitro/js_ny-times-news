905e6936c8c77dc34e10d8488be60a820dbc7bfc
Myroslava Lukash <miralukash@gmail.com>
2023-03-26 17:23:16 +0300

    add calendar

--- src/js/date.js
+++ src/js/date.js
@@ -1,1 +1,11 @@
+const picker = datepicker('#js-datepicker', {
+  language: 'ua',
+  formatter: (input, date) => {
+    input.value = date.toLocaleDateString()
+  },
+  onSelect: (instance, date) => {
+    instance.setDate(date); 
+  }
+});
 
+picker.setDate(new Date());
