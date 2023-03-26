76c49b64d106de4b5174e4e282bf59eaa8f78dae
Myroslava Lukash <miralukash@gmail.com>
2023-03-26 17:43:49 +0300

    change format of the date

--- src/js/date.js
+++ src/js/date.js
@@ -1,7 +1,11 @@
 const picker = datepicker('#js-datepicker', {
   language: 'ua',
   formatter: (input, date) => {
-    input.value = date.toLocaleDateString()
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const formattedMonth = month < 10 ? `0${month}` : month;
+    input.value = `${day}/${formattedMonth}/${year}`;
   },
   onSelect: (instance, date) => {
     instance.setDate(date); 
