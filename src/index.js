eab5fee2ebab41b5d52074b5a2413cf0351742e5
Валентина Сіра <113180300+ValentynaSira1@users.noreply.github.com>
2023-03-27 12:54:51 +0300

    виправила баги

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createHomePageNews } from './js/createHomePageNews';
 import { createHomePageSeachingNews } from "./js/createHomePageSeachingNews";
-
+import { Calendar } from './js/calendar';
 
 createHomePageNews();
