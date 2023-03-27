e869a6e74e1a1445659b3ff5ae1e596b8951ace6
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-03-27 19:03:52 +0300

    1

--- src/js/headerSearchForm.js
+++ src/js/headerSearchForm.js
@@ -5,8 +5,6 @@ const searchBtn = document.querySelector('.search-button');
 searchBtn.addEventListener("click", createHomePageNews);
 document.getElementById("search").addEventListener("input",fetchSeachNews);
 
-// function myFunction() {
-//     // document.getElementById("").innerHTML = "";
-//     let val = this.value.trim();
-// }
+
+
 
