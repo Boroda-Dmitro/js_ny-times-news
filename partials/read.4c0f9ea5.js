8d8de151aeb1ade534930b962d9595f8ebe4872e
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-03-28 12:55:41 +0000

    Deploying to gh-pages from @ Boroda-Dmitro/js_ny-times-news@edf345add3f0e954b38e713a3bf8deb58fccee45 🚀

--- partials/read.4c0f9ea5.js
+++
@@ -1,2 +0,0 @@
-function setTheme(e){localStorage.setItem("theme",e),document.documentElement.className=e}function toggleTheme(){"dark"===localStorage.getItem("theme")?setTheme("light"):setTheme("dark")}"dark"===localStorage.getItem("theme")?setTheme("dark"):setTheme("light");
-//# sourceMappingURL=read.4c0f9ea5.js.map
