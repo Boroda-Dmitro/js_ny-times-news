c50fd21dc2d644541586622734ba060d76cfe915
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-03-29 00:42:49 +0300

    switcher without positioning

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,9 +1,10 @@
 const toggleBtn = document.querySelector('.toggle-button');
+const btnLight = document.querySelector('.toggle-light');
+const btnDark = document.querySelector('.toggle-dark');
     function setTheme(themeName) {
         localStorage.setItem('theme', themeName);
         document.documentElement.className = themeName;
     }
-
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'dark') {
         setTheme('light');
