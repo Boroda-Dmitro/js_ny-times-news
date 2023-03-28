1ac1b74d4b29f7554fc4255accf4633cfe8b898c
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-03-28 14:56:45 +0300

    add eventlisner

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@ import { Calendar } from './js/calendar';
 
 createHomePageNews();
 
-// createHomePageSeachingNews('batman');
+const searchBtn = document.querySelector('.search-button svg');
+
+searchBtn.addEventListener('click', () => {
+  createHomePageSeachingNews('batman');
+});
+
+const input = document.querySelector('.search-input');
+
+console.log(input);
+
+input.addEventListener('input', e => {
+  console.log(e);
+});
