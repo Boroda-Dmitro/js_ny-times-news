c9512abfddac802dab25e223a346d6f9d887c40f
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-03-28 15:53:13 +0300

    favicon 1 page

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,20 +1,20 @@
-
+const toggleBtn = document.querySelector('.toggle-button');
     function setTheme(themeName) {
         localStorage.setItem('theme', themeName);
         document.documentElement.className = themeName;
     }
 
-    function toggleTheme() {
-    if (localStorage.getItem('theme') === 'dark'){
-        setTheme('light');
-    } else {
-        setTheme('dark');
-    }
-    }
-    (function () {
+function toggleTheme() {
     if (localStorage.getItem('theme') === 'dark') {
-        setTheme('dark');
-    } else {
         setTheme('light');
+    } else {
+        setTheme('dark');
     }
+}
+    (function () {
+        if (localStorage.getItem('theme') === 'dark') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
     })();
