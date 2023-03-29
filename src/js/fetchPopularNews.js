478e848690ee9b75fa7a83184051faaf3100e958
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-03-29 18:22:24 +0300

    fix key

--- src/js/fetchPopularNews.js
+++ src/js/fetchPopularNews.js
@@ -1,6 +1,6 @@
 export const fetchPopularNews = () => {
   return fetch(
-    `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=eQ8t8FWqeAGnKDTtIFrHmgZCflFrUTcV`
+    `https://api.nytimes.com/svc/mostpopular/v2/emailed/1.json?api-key=SDNZee2lZmjshzcMGajKH4vNi11jfCZr`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
